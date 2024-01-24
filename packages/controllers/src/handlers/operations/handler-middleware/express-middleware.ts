4ddb6972e2d72cbbcf552d741d3ae673235f29e1
SunsetFi <sunsetfidev@gmail.com>
2024-01-24 09:30:08 -0700

    Fix errors leaking through express middleware

--- packages/controllers/src/handlers/operations/handler-middleware/express-middleware.ts
+++ packages/controllers/src/handlers/operations/handler-middleware/express-middleware.ts
@@ -12,43 +12,43 @@ export function convertExpressMiddleware(
       try {
         return await next();
       } catch (e) {
-        const deferred = new Deferred<any>();
+        return new Promise((accept, reject) => {
+          (expressHandler as ErrorRequestHandler)(
+            e,
+            context.req,
+            context.res,
+            async (err) => {
+              if (err) {
+                reject(err);
+                return;
+              }
 
-        (expressHandler as ErrorRequestHandler)(
-          e,
-          context.req,
-          context.res,
-          async (err) => {
-            if (err) {
-              deferred.reject(err);
-              return;
-            }
-
-            // Error handling middleware is intended to either handle or re-throw the error.
-            // if we completed without error, then the response has been handled and
-            // there is nothing left to do.
-            deferred.resolve(undefined);
-          },
-        );
-
-        return deferred.promise;
+              // Error handling middleware is intended to either handle or re-throw the error.
+              // if we completed without error, then the response has been handled and
+              // there is nothing left to do.
+              accept(undefined);
+            },
+          );
+        });
       }
     };
   } else if (expressHandler.length === 3) {
     return (context, next) => {
-      const deferred = new Deferred<any>();
+      return new Promise((accept, reject) => {
+        (expressHandler as Handler)(context.req, context.res, async (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
 
-      (expressHandler as Handler)(context.req, context.res, async (err) => {
-        if (err) {
-          deferred.reject(err);
-          return;
-        }
-
-        const nextResult = await next();
-        deferred.resolve(nextResult);
+          try {
+            const nextResult = await next();
+            accept(nextResult);
+          } catch (err) {
+            reject(err);
+          }
+        });
       });
-
-      return deferred.promise;
     };
   } else {
     throw new Error(
--- packages/controllers/src/tests/express.e2e.test.ts
+++ packages/controllers/src/tests/express.e2e.test.ts
@@ -1,11 +1,14 @@
 import { OpenAPIObject } from "openapi3-ts/oas31";
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { ImATeapot } from "http-errors";
 
-import { Controller, Get, Req, Res } from "../decorators";
+import { Controller, Get, Req, Res, UseHandlerMiddleware } from "../decorators";
 import { createOpenAPIFromControllers } from "../openapi";
 
 import { createRouterFromSpec } from "../routes";
 import { getMockReq, getMockRes } from "./mocks";
+import { convertExpressMiddleware } from "../handlers";
+import { expectNextCalledWithError } from "./expects";
 
 describe("E2E: Express decorators", function () {
   var handler = jest.fn();
@@ -21,6 +24,30 @@ describe("E2E: Express decorators", function () {
       handler(req, res);
 
       return { bar: true };
+    }
+
+    @Get("/throws-middleware")
+    @UseHandlerMiddleware(
+      convertExpressMiddleware(
+        (req: Request, res: Response, next: NextFunction) => {
+          throw new ImATeapot("Oh no!");
+        },
+      ),
+    )
+    throwsRequest() {
+      return {};
+    }
+
+    @Get("/throws-through-middleware")
+    @UseHandlerMiddleware(
+      convertExpressMiddleware(
+        (req: Request, res: Response, next: NextFunction) => {
+          next();
+        },
+      ),
+    )
+    throwsThroughMiddleware() {
+      throw new ImATeapot("Oh no!");
     }
   }
 
@@ -45,6 +72,42 @@ describe("E2E: Express decorators", function () {
         expect(next).not.toHaveBeenCalled();
 
         expect(handler).toHaveBeenCalledWith(req, res);
+
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }, 10);
+  });
+
+  it("captures errors thrown from express middleware", function (done) {
+    const req = getMockReq("GET", "/throws-middleware");
+    const { res, next } = getMockRes();
+
+    router(req, res, next);
+
+    // Even with sync functions, we await promises, which trampolines us out
+    setTimeout(() => {
+      try {
+        expectNextCalledWithError(next, ImATeapot, /Oh no!/);
+
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }, 10);
+  });
+
+  it("captures errors that propogate through express middleware", function (done) {
+    const req = getMockReq("GET", "/throws-through-middleware");
+    const { res, next } = getMockRes();
+
+    router(req, res, next);
+
+    // Even with sync functions, we await promises, which trampolines us out
+    setTimeout(() => {
+      try {
+        expectNextCalledWithError(next, ImATeapot, /Oh no!/);
 
         done();
       } catch (e) {
